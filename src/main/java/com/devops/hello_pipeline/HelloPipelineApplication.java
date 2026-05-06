e3e2fcdea592262c7204a9d64f0d208f386bc45a
Meenakshi <meenakshiselvaraj@gmail.com>
2026-05-06 23:28:10 +0530

    Initial project setup

---
+++ src/main/java/com/devops/hello_pipeline/HelloPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.devops.hello_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloPipelineApplication.class, args);
+	}
+
+}
